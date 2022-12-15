25ad644fee212e668918699a2c5c6acd9d188c1d
Chantee <Chantee.hoarau123@gmail.com>
2022-12-15 12:37:21 +0200

    Add a function that plays a single round of Rock Paper Scissors

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ function getComputerChoice(){
 }
 
 getComputerChoice();
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection === computerSelection){
+        return 'Tie!';
+    }else if(playerSelection === 'Rock' && computerSelection === 'Paper'){
+        return 'You lose! Paper beats Rock';
+    }else if(playerSelection === 'Paper' && computerSelection === 'Rock'){
+        return 'You win! Paper beats Rock';
+    }else if(playerSelection === 'Paper' && computerSelection === 'Scissors'){
+        return 'You lose! Scissors beats Paper';
+    }else if(playerSelection === 'Scissors' && computerSelection === 'Paper'){
+        return 'You win! Scissors beats Paper';
+    }else if(playerSelection === 'Scissors' && computerSelection === 'Rock'){
+        return 'You lose! Rock beats Scissors';
+    }else if(playerSelection === 'Rock' && computerSelection === 'Scissors'){
+        return 'You win! Rock beats Scissors';
+    }
+}
